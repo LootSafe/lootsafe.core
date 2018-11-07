42b7e79778d895dc479e8b67679d8eb5c6af4ab6
Matt Wisniewski <contact.mattdylan@gmail.com>
2018-11-07 03:06:33 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         registry: '0xb94dc475fb2c8fd8c1381f163c59dbc570c500d0',
         buildDir: `${__dirname}/api/contracts/core/build/contracts`
     },
-    port: 1007,
+    port: 1337,
     version: 1,
     dbURI: 'mongodb://localhost/lscore',
     defaultAccount: '0xbBEF6Be732404c372153241cFB1d3213a054A2A8',
